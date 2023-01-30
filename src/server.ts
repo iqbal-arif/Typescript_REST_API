feaa718b3a5eae3d24dbc6bd564cd8c770e73c93
Arif Iqbal <arif_iqbal@yahoo.com>
2023-01-30 15:17:49 -0600

    modified package modified server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,30 @@
-console.log('Hello World');
+/************IMPORTS */
+/**Express Module from /node_modules */
+import * as express from 'express';
+// import { root } from './routes/route';
+
+/***Initialize Express */
+const app = express();
+
+/********* FUNCTION SETUP EXPRESS
+Express Routes and Express Middleware
+*/
+function setupExpress() {
+  /****Express EndPoint aka Routes */
+  /****Route is a mapping between URL accessing in endpoint and Request Handler processes the response to the request */
+  /**Route Request Handler */
+  //   app.route('/').get(root);
+}
+
+/********* FUNCTION START SERVER
+ Logic to Start Server
+ */
+function startServer() {
+  app.listen(9000, () => {
+    console.log('HTTP REST API Server is now running at http://localhost:9000');
+  });
+}
+
+/********* INVOKING THE FUNCTIONS *******/
+setupExpress();
+startServer();
