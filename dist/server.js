5f8faf2c011bf590fc4fe3f39d9b1a467077eaff
Arif Iqbal <arif_iqbal@yahoo.com>
2023-01-30 19:00:43 -0600

    modified .env, server, package

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+/************IMPORTS ENVIRONMENT DOTENV MODULE*/
+var dotenv = require("dotenv");
+//Loads .env file contents into process.env.
+var result = dotenv.config();
+// Sanity Check for ENV data
+if (result.error) {
+    console.log("Error loading environment variables, aborting.");
+    process.exit(1);
+}
+console.log(process.env.PORT);
 /************IMPORTS */
 /**Express Module from /node_modules */
 var express = require("express");
@@ -24,14 +34,21 @@ function startServer() {
     //   console.log(process.argv); /*See Note 18 in REST_API_ReadMe.md*/
     // process.argv picks argument from package.json script declaration.
     var port;
+    /*******Importing the PORT from ".evn" file*******/
+    var portEnv = process.env.PORT;
+    /*******Importing the PORT from "package.json script via process.argv" file*******/
     var portArg = process.argv[2]; //index [2] because it contains PORT NUMBER.
+    /****isInteger Check */
+    if ((0, utils_1.isInteger)(portEnv)) {
+        port = parseInt(portEnv);
+    }
     // Converting String to Number
     /*****Important NOTE ON parseInt*****/
     /*
       1. parsInt("9001"): result is 9001
       1. parsInt("asdfjkl"): result is NaN
       1. parsInt("9001asdafjkl"): result is 9001 (It takes the number part only*/
-    if ((0, utils_1.isInteger)(portArg)) {
+    if (!port && (0, utils_1.isInteger)(portArg)) {
         port = parseInt(portArg);
     }
     //   if port is not defined or has
